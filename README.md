2f12cdc7c5e94a66b86792858324860708171cdf
Kee Boon Hwee <2717921K@student.gla.ac.uk>
2023-02-21 23:20:32 +0800

    update instructions

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # CSC2008Database
+
+(1) execute
+./Untrack_README_txt.bat
+
+(2) Remove all README.txt from
+    NoSQL/configdb/README.txt
+    NoSQL/db/README.txt
+    SQL/mysql/README.txt
+    SQL/mysql-files/README.txt
+
+(3) execute
+docker compose up
---
+++ Untrack_README_txt.bat
@@ -0,0 +1,4 @@
+git update-index --assume-unchanged ./NoSQL/configdb/README.txt
+git update-index --assume-unchanged ./NoSQL/db/README.txt
+git update-index --assume-unchanged ./SQL/mysql/README.txt
+git update-index --assume-unchanged ./SQL/mysql-files/README.txt
