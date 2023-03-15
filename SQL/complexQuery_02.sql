6e248de3387ca97bfe88ceb39b0c01707efb704b
NgShiYingNicole <97488614+NgShiYingNicole@users.noreply.github.com>
2023-03-15 22:13:53 +0800

    Update complexQuery_02.sql
    update query to stored procedure

--- SQL/complexQuery_02.sql
+++ SQL/complexQuery_02.sql
@@ -1,6 +1,12 @@
-SELECT songs.title, songs.top_genre, GROUP_CONCAT(DISTINCT albums.album) AS album , GROUP_CONCAT(DISTINCT artists.artist) AS artists
-FROM songs
-JOIN albums ON songs.title = albums.name
-JOIN artists ON albums.artists LIKE CONCAT('%',artists.artist ,'%')
-WHERE artists.artist = 'Adele'
-GROUP BY songs.title, songs.top_genre;
+DELIMITER $
+CREATE PROCEDURE spotify.FindSongsAndAlbumsByArtistName( IN artistName VARCHAR(100) )
+BEGIN
+  SELECT songs.title, songs.top_genre, GROUP_CONCAT(DISTINCT albums.album) AS album , GROUP_CONCAT(DISTINCT artists.artist) AS artists
+  FROM songs
+  JOIN albums ON songs.title = albums.name
+  JOIN artists ON albums.artists LIKE CONCAT('%',artists.artist ,'%')
+  WHERE artists.artist = artistName
+  GROUP BY songs.title, songs.top_genre;
+END$
+DELIMITER ;
+CALL FindSongsAndAlbumsByArtistName('Adele');
