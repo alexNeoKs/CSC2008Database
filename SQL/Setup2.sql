f7690bb5679e5ce6a0a6a816059b9b53339c6fd7
Kee Boon Hwee <2717921K@student.gla.ac.uk>
2023-03-18 11:03:36 +0800

    update setup2 to include primary ketys creation

--- SQL/Setup2.sql
+++ SQL/Setup2.sql
@@ -1,3 +1,6 @@
+SET SQL_SAFE_UPDATES = 0;
+USE spotify;
+
 drop table if exists album;
 CREATE TABLE album AS 
 (SELECT distinct album_id, album from tmp_albums);
@@ -61,3 +64,58 @@ CREATE TABLE song AS
 drop table if exists gender;
 CREATE TABLE gender AS
 (SELECT distinct artist, gender, `type` from tmp_artists);
+
+ALTER TABLE acousticness MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE acousticness ADD PRIMARY KEY (song_id);
+
+ALTER TABLE danceability MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE danceability ADD PRIMARY KEY (song_id);
+
+ALTER TABLE energy MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE energy ADD PRIMARY KEY (song_id);
+
+ALTER TABLE explicit MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE explicit ADD PRIMARY KEY (song_id);
+
+ALTER TABLE instrumentalness MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE instrumentalness ADD PRIMARY KEY (song_id);
+
+ALTER TABLE `key` MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE `key` ADD PRIMARY KEY (song_id);
+
+ALTER TABLE liveness MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE liveness ADD PRIMARY KEY (song_id);
+
+ALTER TABLE loudness MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE loudness ADD PRIMARY KEY (song_id);
+
+ALTER TABLE `mode` MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE `mode` ADD PRIMARY KEY (song_id);
+
+ALTER TABLE song MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE song MODIFY COLUMN name    VARCHAR(600);
+ALTER TABLE song ADD PRIMARY KEY (song_id);
+
+ALTER TABLE song_album MODIFY COLUMN song_id  VARCHAR(22);
+ALTER TABLE song_album MODIFY COLUMN album_id VARCHAR(22);
+ALTER TABLE song_album ADD PRIMARY KEY (song_id);
+
+ALTER TABLE speechiness MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE speechiness ADD PRIMARY KEY (song_id);
+
+ALTER TABLE tempo MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE tempo ADD PRIMARY KEY (song_id);
+
+ALTER TABLE valence MODIFY COLUMN song_id VARCHAR(22);
+ALTER TABLE valence ADD PRIMARY KEY (song_id);
+
+ALTER TABLE normalized_artists            RENAME artist;
+ALTER TABLE normalized_song_lookup_artist RENAME song_artist;
+
+DELETE FROM artist WHERE artist_id = '1G9G7WwrXka3Z1r7aIDjI7';
+ALTER TABLE artist ADD PRIMARY KEY (artist_id);
+INSERT INTO artist VALUES ( '1G9G7WwrXka3Z1r7aIDjI7' , 'Outkast' );
+
+ALTER TABLE song_artist ADD PRIMARY KEY (song_id,artist_id);
+
+
