062bd8b3bcd876e91a984f9100de1c24ece5b366
Kee Boon Hwee <2717921K@student.gla.ac.uk>
2023-03-25 09:52:26 +0800

    update .bat file

--- Untrack_README_txt.bat
+++ Untrack_README_txt.bat
@@ -2,3 +2,8 @@ git update-index --assume-unchanged ./NoSQL/configdb/README.txt
 git update-index --assume-unchanged ./NoSQL/db/README.txt
 git update-index --assume-unchanged ./SQL/mysql/README.txt
 git update-index --assume-unchanged ./SQL/mysql-files/README.txt
+
+rm "./NoSQL/configdb/README.txt"
+rm "./NoSQL/db/README.txt"
+rm "./SQL/mysql/README.txt"
+rm "./SQL/mysql-files/README.txt"
